dff1e81074d50d16455adf442025b35c5197ac2b
Coder2195 <coder2195mail@gmail.com>
2025-12-29 00:09:22 -0500

    style changes

--- src/components/ui/navbar/index.tsx
+++ src/components/ui/navbar/index.tsx
@@ -6,15 +6,16 @@ import ThemeButton from "./theme";
 
 const Navbar: FC = () => {
   return (
-    <nav className="flex gap-2 bg-primary p-2 justify-between items-center text-white fixed top-0 left-0 w-full z-50">
+    <nav className="flex gap-2 bg-primary/70 backdrop-blur-md p-2 justify-between items-center text-white fixed top-2 left-2 w-[calc(100%-1rem)] z-50 rounded-lg">
       <Link className="flex gap-2 items-center font-bold " href="/">
         <Image
           src="/pawprints.svg"
           alt="Pawprints Logo"
-          width={40}
-          height={40}
+          width={32}
+          height={32}
+          className="w-8 h-8"
         />
-        Pawprints
+        <span className="3xs:inline hidden text-lg">Pawprints</span>
       </Link>
       <div className="flex-1"></div>
       <ThemeButton />
